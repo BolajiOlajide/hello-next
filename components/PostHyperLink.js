31059dab41ea31f277b34b3a1bab7c41c7b57fca
BolajiOlajide <bolaji.olajide@andela.com>
2017-12-08 02:03:26 +0100

    implement route masking

--- components/PostHyperLink.js
+++ components/PostHyperLink.js
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 
-const PostLink = (props) => (
+const PostLink = (props) => {
+  console.log(props.id)
+  return (
   <li>
-    <Link href={`/post?title=${props.title}`}>
+    <Link as={`/posts/${props.id}`} href={`/post?title=${props.title}`}>
       <a>{props.title}</a>
     </Link>
   </li>
-);
+)};
 
 export default PostLink;
