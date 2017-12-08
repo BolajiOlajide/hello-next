fbbf15389c14b7596abc79fae8e6e63da5e500ec
BolajiOlajide <bolaji.olajide@andela.com>
2017-12-08 01:43:31 +0100

    create post link component

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -1,9 +1,10 @@
 import Header from './Header';
 
 const layoutStyle = {
-  margin: 20,
-  padding: 20,
-  border: '1px solid #DDD'
+  margin: 70,
+  padding: 40,
+  border: '2px solid #000',
+  backgroundColor: 'beige'
 };
 
 const Layout = (props) => (
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,16 @@ import Link from 'next/link';
 
 // components
 import Container from '../components/MyLayout';
+import PostLink from '../components/PostHyperLink';
 
 const Index = () => (
   <Container>
-    <p>Hello Next.js</p>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js"/>
+      <PostLink title="Learn Next.js is awesome"/>
+      <PostLink title="Deploy apps with Zeit"/>
+    </ul>
   </Container>
 );
 
